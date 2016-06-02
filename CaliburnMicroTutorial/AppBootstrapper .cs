aa5ae54a459580160dc2d7d29f408182df0b2052
Filip Paluch <filip.paluch@o2.com>
2016-06-02 08:59:41 +0200

    CaliburnMicro tutorial

---
+++ CaliburnMicroTutorial/AppBootstrapper .cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+
+namespace CaliburnMicroTutorial
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<TestViewModel>();
+        }
+    }
+}
---
+++ CaliburnMicroTutorial/TestViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using RandomNameGenerator;
+
+namespace CaliburnMicroTutorial
+{
+    public class TestViewModel : PropertyChangedBase
+    {
+        private string _name = "Philip";
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyOfPropertyChange(() => Name);
+            }
+        }
+
+        public void NameChanged()
+        {
+            Name = NameGenerator.Generate(Gender.Male);
+        }
+    }
+}
